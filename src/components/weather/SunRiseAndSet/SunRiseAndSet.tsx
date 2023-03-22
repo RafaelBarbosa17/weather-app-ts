315a96adcd474f7da9bf442065bffc183babe0b2
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2023-03-22 12:19:39 -0300

    melhoria no grafico de horário de sol v2.5

--- src/components/weather/SunRiseAndSet/SunRiseAndSet.tsx
+++ src/components/weather/SunRiseAndSet/SunRiseAndSet.tsx
@@ -21,19 +21,67 @@ export const SunRiseAndSet = (props: {sras: SAS}) => {
 
     const timeSunInSky:number = props.sras.sunsetHour - props.sras.sunriseHour;
     // console.log(timeSunInSky)
-    const hour = new Date().getHours();
-
+    let hour = new Date().getHours();
+    hour = 10
     const sunInSky = {
         height: '100%',
         width: `${100 / timeSunInSky * (hour - props.sras.sunriseHour)}%`
+    }
+
+    const definteIconPos = () => {
+        if (hour === 5) {
+            return '60px'
+        }
+        else if (hour === 6) {
+            return '30px'
+        }
+        else if (hour === 7) {
+            return '15px'
+        }
+        else if (hour === 8) {
+            return '5px'
+        }
+        else if (hour === 9) {
+            return '-5px'
+        }
+        else if (hour === 10) {
+            return '-10px'
+        } 
+        else if (hour === 11 || hour === 12) {
+            return '-12px'
+        }
+        else if (hour === 13) {
+            return '-10px'
+        }
+        else if (hour === 14) {
+            return '-5px'
+        }
+        else if (hour === 15) {
+            return '5px'
+        }
+        else if (hour === 16) {
+            return '15px'
+        }
+        else if (hour === 17) {
+            return '30px'
+        }
+        else if (hour === 18) {
+            return '60px'
+        }
+    }
+
+    const iconStyle = {
+        translate: `12px  ${definteIconPos()}`
     }
 
     return (
         <div className="SunRiseAndSet">
             <h1 className="sras-title">Horário de Sol</h1>
             <div className="grafic-box">
-                <div className="grafic-item" style={sunInSky}>
-                    <BsFillSunFill />
+                <div className="grafic-item">
+                    <div className="grafic-selected" style={sunInSky}>
+                        <BsFillSunFill style={iconStyle} />
+                    </div>
                 </div>
             </div>
             <div className="times">
